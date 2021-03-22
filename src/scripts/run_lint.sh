dbc03e944d53e8c4b9f10959bb9deba52d0b0889
Kyle Tryon <Kyle.Tryon@outlook.com>
2021-03-22 16:52:46 -0400

    debug

--- src/scripts/run_lint.sh
+++ src/scripts/run_lint.sh
@@ -3,7 +3,6 @@ PATH="${PATH}:/node_modules/.bin:/var/cache/dotnet/tools:/usr/share/dotnet"
 # Set directory
 DEFAULT_WORKSPACE=$(echo "$PWD/$SCAN_DIR")
 export DEFAULT_WORKSPACE
-echo Scanning $DEFAULT_WORKSPACE
 # Create report directory
 mkdir "$PWD/$SCAN_DIR/super-linter.report"
 # Run Linter
@@ -11,10 +10,14 @@ bash /action/lib/linter.sh
 # Copy reports for artifacts
 cp -R "$PWD/$SCAN_DIR/super-linter.report" /tmp/super-linter.report
 # Convert reports to JUNIT
+echo Converting Test Results to JUNIT
+cd /tmp/super-linter.report || exit 1
+ls
 npm install -g tap-junit
 FILES=/tmp/super-linter.report/*
 for f in $FILES
 do
     FILENAME=$(basename "$f")
+    echo "Converting file: $FILENAME from $f"
     tap-junit -o /tmp/test_results -n "$FILENAME" -s "$FILENAME" -i "$f"
 done
