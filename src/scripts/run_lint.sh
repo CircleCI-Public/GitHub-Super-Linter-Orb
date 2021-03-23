bc5328b9dc973033f0fcc7f69bb81aa98813e9a3
Kyle Tryon <Kyle.Tryon@outlook.com>
2021-03-23 12:13:41 -0400

    + suite/class names / move test results under artifacts

--- src/scripts/run_lint.sh
+++ src/scripts/run_lint.sh
@@ -10,24 +10,14 @@ set +e
 bash /action/lib/linter.sh
 set -e
 # Copy reports for artifacts
-echo "State of /tmp/artifacts/reports before copy: "
-ls /tmp/artifacts/reports
 cp -a "$PWD/$SCAN_DIR/super-linter.report/." /tmp/artifacts/reports
-echo "State of /tmp/artifacts/reports after copy:"
-ls /tmp/artifacts/reports
 # Convert reports to JUNIT
 echo Converting Test Results to JUNIT
-echo "Test Report Files:"
-ls /tmp/artifacts/reports
-echo Installing tap-junit
 npm install -g tap-junit
 FILES=(/tmp/artifacts/reports/*)
-echo "Found Files: ${FILES[*]}"
 for f in "${FILES[@]}"
 do
     NAME=$(basename "$f" .tap)
     echo "Converting file: $NAME from $f"
-    tap-junit -o /tmp/test_results -n "$NAME" < "$f"
+    tap-junit -o /tmp/artifacts/test_results -n "$NAME" -s "$NAME" -c "$NAME" < "$f"
 done
-# Copy tests for artifacts
-cp -a /tmp/test_results/. /tmp/artifacts/test_results
