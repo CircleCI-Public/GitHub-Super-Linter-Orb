453c224b84250a96c7865d0821ccb33a834fdda9
Kyle Tryon <Kyle.Tryon@outlook.com>
2021-03-23 10:02:03 -0400

    More debugging

--- src/scripts/run_lint.sh
+++ src/scripts/run_lint.sh
@@ -1,7 +1,7 @@
 # Load Paths
 PATH="${PATH}:/node_modules/.bin:/var/cache/dotnet/tools:/usr/share/dotnet"
 # Set directory
-DEFAULT_WORKSPACE=$(echo "$PWD/$SCAN_DIR")
+DEFAULT_WORKSPACE="$PWD/$SCAN_DIR"
 export DEFAULT_WORKSPACE
 # Create artifact directory
 mkdir -p "/tmp/artifacts/test_results" "/tmp/artifacts/reports" "/tmp/test_results"
@@ -10,14 +10,19 @@ set +e
 bash /action/lib/linter.sh
 set -e
 # Copy reports for artifacts
-cp -R "$PWD/$SCAN_DIR/super-linter.report" /tmp/artifacts/reports
+echo "State of /tmp/artifacts/reports before copy: "
+ls /tmp/artifacts/reports
+cp "$PWD/$SCAN_DIR/super-linter.report" /tmp/artifacts/reports
+echo "State of /tmp/artifacts/reports after copy: "
+ls /tmp/artifacts/reports
 # Convert reports to JUNIT
 echo Converting Test Results to JUNIT
-echo "Test Result Files:"
-ls
+echo "Test Report Files:"
+ls /tmp/artifacts/reports
 echo Installing tap-junit
 npm install -g tap-junit
 FILES=(/tmp/artifacts/reports/*)
+echo "Found Files: ${FILES[*]}"
 for f in "${FILES[@]}"
 do
     FILE=$(basename "$f")
@@ -26,4 +31,4 @@ do
     tap-junit -o /tmp/test_results -n "$NAME" -i "$FILE"
 done
 # Copy tests for artifacts
-cp -R /tmp/test_results /tmp/artifacts/test_results
+cp /tmp/test_results /tmp/artifacts/test_results
