364531fd52bf516ee2b46968b12fec3d73ef77a8
Kyle Tryon <Kyle.Tryon@outlook.com>
2021-03-23 17:14:26 -0400

    remove artifact storage

--- src/scripts/run_lint.sh
+++ src/scripts/run_lint.sh
@@ -6,12 +6,4 @@ export DEFAULT_WORKSPACE
 # Create artifact directory
 mkdir -p "$PWD/$SCAN_DIR/super-linter.report/" "/tmp/artifacts/reports"
 # Run Linter
-set +e
 bash /action/lib/linter.sh
-LINTER_ERROR_STATUS=$?
-# Copy reports for artifacts
-echo "Copying reports to artifacts"
-cp -a "$PWD/$SCAN_DIR/super-linter.report/." /tmp/artifacts/reports
-cp "$PWD/$SCAN_DIR/super-linter.log" /tmp/artifacts/logs
-# Exit with proper exit code
-exit "$LINTER_ERROR_STATUS"
