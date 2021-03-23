6aed3e877773b8f8fdb1d8af613d8a30a641c455
Kyle Tryon <Kyle.Tryon@outlook.com>
2021-03-23 12:57:22 -0400

    remove junit and test results

--- src/scripts/run_lint.sh
+++ src/scripts/run_lint.sh
@@ -8,20 +8,5 @@ mkdir -p "/tmp/artifacts/test_results" "/tmp/artifacts/reports" "/tmp/test_resul
 # Run Linter
 set +e
 bash /action/lib/linter.sh
-set -e
 # Copy reports for artifacts
 cp -a "$PWD/$SCAN_DIR/super-linter.report/." /tmp/artifacts/reports
-# Convert reports to JUNIT
-echo Converting Test Results to JUNIT
-npm install -g tap-junit
-export FILES=(/tmp/artifacts/reports/*)
-echo "Conversion list: "
-echo "${FILES[*]}"
-echo "${#FILES[@]} files found"
-for f in "${FILES[@]}"
-do
-    set +e
-    NAME=$(basename "$f" .tap)
-    echo "Converting file: $NAME from $f"
-    tap-junit -o /tmp/artifacts/test_results -n "$NAME" -s "$NAME" -c "$NAME" < "$f"
-done
