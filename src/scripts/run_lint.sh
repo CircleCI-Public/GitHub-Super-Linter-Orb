afe7a6f7d7083a460028099bab3e55fe3f607304
Kyle Tryon <Kyle.Tryon@outlook.com>
2021-03-23 14:46:49 -0400

    remove set +e

--- src/scripts/run_lint.sh
+++ src/scripts/run_lint.sh
@@ -6,7 +6,6 @@ export DEFAULT_WORKSPACE
 # Create artifact directory
 mkdir -p "$PWD/$SCAN_DIR/super-linter.report/" "/tmp/artifacts/reports"
 # Run Linter
-set +e
 bash /action/lib/linter.sh
 # Copy reports for artifacts
 cp -a "$PWD/$SCAN_DIR/super-linter.report/." /tmp/artifacts/reports
